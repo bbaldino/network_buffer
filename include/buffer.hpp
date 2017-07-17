d2429ae4b18dec3f4ad91e1caf969c3f3a463321
brian baldino <bbaldino@gmail.com>
2017-07-17 16:15:39 -0700

    initial commit

---
+++ include/buffer.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+/**
+ * Stores data in a buffer in network order, provides
+ * convenience methods for writing to and reading
+ * from the buffer
+ */
+class NetworkBuffer {
+public:
+    NetworkBuffer() :
+        _head(_buffer), _tail(_buffer) {}
+
+    void write(const uint8_t& val) {
+        _write(val);
+    }
+
+    void write(const uint16_t& val) {
+        uint16_t networkVal = htons(val);
+        _write(networkVal);
+    }
+
+    void write(const uint32_t& val) {
+        uint32_t networkVal = htonl(val);
+        _write(networkVal);
+    }
+
+    uint8_t read8() const {
+        return _read<uint8_t>();
+    }
+
+    uint16_t read16() const {
+        uint16_t res = _read<uint16_t>();
+        return ntohs(res);
+    }
+
+    uint32_t read32() const {
+        uint32_t res = _read<uint32_t>();
+        return ntohl(res);
+    }
+
+    const uint8_t* const getBuffer() const {
+        return _buffer;
+    }
+
+    /**
+     * Get a non-const version of the buffer (to be used, 
+     * for example, when reading from the network)
+     */
+    uint8_t* getBuffer() {
+        return _buffer;
+    }
+
+    /**
+     * Manually set the size of the buffer
+     * NOTE: this should *only* be used when the internal
+     * buffer has been grabbed (via getBuffer) and written
+     * to directly.
+     * TOOD: is there a better way to implement this?
+     */
+    void setSize(std::size_t size) {
+        assert(size <= BUF_SIZE);
+        _tail += size;
+    }
+
+    /**
+     * Returns the amount of bytes written to the buffer
+     * NOTE: this will include all bytes written and
+     * will not exclude any bytes that may have been read
+     * from the head of the buffer
+     */
+    size_t size() const {
+        return _tail - _buffer;
+    }
+
+    static const std::size_t BUF_SIZE = 1500;
+
+//protected:
+    uint8_t _buffer[BUF_SIZE]; 
+    mutable uint8_t* _head; // Tracks reading
+    uint8_t* _tail; // Tracks writing
+
+    template<typename T>
+    void _write(const T& val) {
+        assert(_tail + sizeof(T) < _buffer + BUF_SIZE);
+        memcpy(_tail, &val, sizeof(T));
+        _tail += sizeof(T);
+    }
+
+    template<typename T>
+    T _read() const {
+        assert(_head < (_tail + sizeof(T)));
+        T val;
+        memcpy(&val, _head, sizeof(T));
+        _head += sizeof(T);
+        return val;
+    }
+};
---
+++ utest/utest.cc
@@ -0,0 +1,99 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "buffer.hpp"
+
+TEST_CASE("Basic read/write tests") {
+    NetworkBuffer buffer;
+
+    SECTION("uint8_t") {
+        uint8_t writeVal = 42;
+        buffer.write(writeVal);
+        uint8_t readVal = buffer.read8();
+        REQUIRE(readVal == writeVal);
+    }
+
+    SECTION("uint16_t") {
+        uint16_t writeVal = 0xDEAD;
+        buffer.write(writeVal);
+        uint16_t readVal = buffer.read16();
+        REQUIRE(readVal == writeVal);
+    }
+
+    SECTION("uint32_t") {
+        uint32_t writeVal = 0xDEADBEEF;
+        buffer.write(writeVal);
+        uint32_t readVal = buffer.read32();
+        REQUIRE(readVal == writeVal);
+    }
+}
+
+TEST_CASE("Size tests") {
+    NetworkBuffer buffer;
+
+    buffer.write(static_cast<uint8_t>(42));
+    REQUIRE(buffer.size() == 1);
+    buffer.write(static_cast<uint8_t>(42));
+    REQUIRE(buffer.size() == 2);
+    buffer.write(static_cast<uint16_t>(512));
+    REQUIRE(buffer.size() == 4);
+    buffer.write(static_cast<uint32_t>(512 * 512));
+    REQUIRE(buffer.size() == 8);
+    buffer.write(static_cast<uint16_t>(512));
+    REQUIRE(buffer.size() == 10);
+    buffer.write(static_cast<uint8_t>(42));
+    REQUIRE(buffer.size() == 11);
+}
+
+TEST_CASE("Network order") {
+    // Verify that the buffer is storing the data
+    // in network order
+    NetworkBuffer buffer;
+
+    buffer.write(static_cast<uint8_t>(42));
+    buffer.write(static_cast<uint16_t>(512));
+    buffer.write(static_cast<uint32_t>(512 * 512));
+    // Get the raw buffer and directly memcpy out of it
+    const uint8_t* const buf = buffer.getBuffer();
+    uint8_t bufOffset = 0;
+
+    uint8_t networkByte;
+    memcpy(&networkByte, buf + bufOffset, sizeof(uint8_t));
+    bufOffset += sizeof(uint8_t);
+    REQUIRE(networkByte == 42);
+
+    uint16_t networkShort;
+    memcpy(&networkShort, buf + bufOffset, sizeof(uint16_t));
+    bufOffset += sizeof(uint16_t);
+    REQUIRE(networkShort == htons(512));
+
+    uint32_t networkLong;
+    memcpy(&networkLong, buf + bufOffset, sizeof(uint32_t));
+    REQUIRE(networkLong == htonl(512 * 512));
+}
+
+TEST_CASE("Read directly into buffer") {
+    // Make sure that when we read into the buffer directly, we still
+    // get the proper values out
+    uint8_t networkByte = 42;
+    uint16_t networkShort = htons(512);
+    uint32_t networkLong = htonl(512 * 512);
+
+    NetworkBuffer buffer;
+    uint8_t* buf = buffer.getBuffer();
+    memcpy(buf, &networkByte, sizeof(networkByte));
+    buf += sizeof(networkByte);
+    memcpy(buf, &networkShort, sizeof(networkShort));
+    buf += sizeof(networkShort);
+    memcpy(buf, &networkLong, sizeof(networkLong));
+    buf += sizeof(networkLong);
+    buffer.setSize(buf - buffer.getBuffer());
+
+    REQUIRE(buffer.size() == sizeof(networkByte) + sizeof(networkShort) + sizeof(networkLong));
+    uint8_t hostByte = buffer.read8();
+    REQUIRE(hostByte == 42);
+    uint16_t hostShort = buffer.read16();
+    REQUIRE(hostShort == 512);
+    uint32_t hostLong = buffer.read32();
+    REQUIRE(hostLong == 512 * 512);
+}
